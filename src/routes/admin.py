dd5dfb11f9170a507ba26816c5264de03e8c461d
Sidney Francisco <sidneyfranciscodev@gmail.com>
2026-04-13 08:51:26 -0700

    boilerplate admin layout

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -12,7 +12,7 @@ templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 
 @router.get(
-    "/admin/dashboard",
+    "/admin/painel",
     name="admin_dashboard",
     response_class=HTMLResponse
 )
@@ -22,6 +22,66 @@ def dashboard(
 ) -> HTMLResponse:
     return templates.TemplateResponse(
         name='pages/admin/dashboard.html',
+        context={'request': request,},
+        request=request
+    )
+
+@router.get(
+    "/admin/crud/users",
+    name="admin_users",
+    response_class=HTMLResponse
+)
+def users(
+    request: Request,
+    #user = Depends(authenticate)
+) -> HTMLResponse:
+    return templates.TemplateResponse(
+        name='pages/admin/crud.html',
+        context={'request': request},
+        request=request
+    )
+
+@router.get(
+    "/admin/crud/modules",
+    name="admin_modules",
+    response_class=HTMLResponse
+)
+def modules(
+    request: Request,
+    #user = Depends(authenticate)
+) -> HTMLResponse:
+    return templates.TemplateResponse(
+        name='pages/admin/crud.html',
+        context={'request': request},
+        request=request
+    )
+
+@router.get(
+    "/admin/crud/lessons",
+    name="admin_lessons",
+    response_class=HTMLResponse
+)
+def lessons(
+    request: Request,
+    #user = Depends(authenticate)
+) -> HTMLResponse:
+    return templates.TemplateResponse(
+        name='pages/admin/crud.html',
+        context={'request': request},
+        request=request
+    )
+
+@router.get(
+    "/admin/settings",
+    name="admin_settings",
+    response_class=HTMLResponse
+)
+def settings(
+    request: Request,
+    #user = Depends(authenticate)
+) -> HTMLResponse:
+    return templates.TemplateResponse(
+        name='pages/admin/settings.html',
         context={'request': request},
         request=request
     )
